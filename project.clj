b6845a232d9a2abe127748a7bd385cd0dc200572
William Ravel <wravel@gmail.com>
2019-05-21 23:47:40 +0200

    Update

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.suskalo/discljord "0.2.4"]]
+                 [org.suskalo/discljord "0.2.4"]
+                 [clj-http "3.10.0"]
+                 [enlive "1.1.6"]]
   :repl-options {:init-ns robonou.core})
--- src/robonou/core.clj
+++ src/robonou/core.clj
@@ -2,12 +2,33 @@
   (:require [discljord.connections :as c]
             [discljord.messaging :as m]
             [discljord.events :as e]
-            [clojure.core.async :as a]))
+            [clojure.core.async :as a]
+            [clj-http.client :as client]
+            [net.cgrand.enlive-html :as html]))
+
+(defn citation-du-jour []
+  (let [body (:body (client/get "http://evene.lefigaro.fr/citations"))
+        node (html/html-snippet body)]
+    (-> (html/select node [:.figsco__quote__text])
+        first :content first)))
 
 (def settings (clojure.edn/read-string (slurp "data/settings.edn")))
 (def token (:token settings))
 (def state (atom nil))
 (def channel {::bot-test 580121292121047062})
+
+(def command-list-hidden
+  {:command-list-hidden/lewp ""
+   :command-list-hidden/will ""
+   :command-list-hidden/gif "Fais péter un gif"
+   :command-list-hidden/robonou ""
+   :command-list-hidden/clj-eval "Clojure repl eval"})
+
+(def command-list
+  {:command-list/help "Affiche l'aide"
+   :command-list/commands "Affiche les commandes disponibles"
+   :command-list/hidden-commands "A tes risques et périls"
+   :command-list/day-quote "La citation du jour"})
 
 (defmulti command
   (fn [command-name]
@@ -15,8 +36,27 @@
 
 (defmethod command :command-list/help
   [command-name]
-  {:key (name command-name)
-   :message "Bienvenue sur l'aide intéractive, tapez !comands pour afficher la liste des commandes"})
+  "Bienvenue sur l'aide intéractive, tape !comands pour afficher la liste des commandes")
+
+(defmethod command :command-list-hidden/lewp
+  [command-name]
+  "Dommage, Hugo joue à LoL, essayes plus tard")
+
+(defmethod command :command-list-hidden/will
+  [command-name]
+  "Dommage, William dors, essayes plus tard")
+
+(defmethod command :command-list-hidden/gif
+  [command-name]
+  "TODO afficher un gif marrant")
+
+(defmethod command :command-list-hidden/robonou
+  [command-name]
+  "Robonou à ton service")
+
+(defmethod command :command-list-hidden/clj-eval
+  [command-name message]
+  (eval (read-string message)))
 
 (defmethod command :command-list/commands
   [command-name]
@@ -25,20 +65,19 @@
            (str key " - " message)))
        (clojure.string/join "\n")))
 
-(def command-list
-  {:command-list/help "Affiche l'aide"
-   :command-list/commands "Affiche les commandes disponibles"})
+(defmethod command :command-list/hidden-commands
+  [command-name]
+  (->> (for [[key message] command-list-hidden]
+         (let [key (str "!" (name key))]
+           (str key " - " message)))
+       (clojure.string/join "\n")))
+
+(defmethod command :command-list/day-quote
+  [command-name]
+  (citation-du-jour))
 
 (defn command-str [command-name]
   (str "!" (name command-name)))
-
-(comment
-  (m/create-message! (:messaging @state)
-                     (::bot-test channel)
-                     :content "Salut")
-
-  @(m/get-channel! (:messaging @state) (::bot-test channel)))
-
 
 (defmulti handle-event
   (fn [event-type event-data]
@@ -49,19 +88,11 @@
 
 (defmethod handle-event :message-create
   [event-type {{bot :bot} :author :keys [channel-id content]}]
-  (cond 
-    (= content "!disconnect")
-    (a/put! (:connection @state) [:disconnect])
-
-    (= content (command-str :command-list/commands))
-    (when-not bot
-      (m/create-message! (:messaging @state) channel-id :content (command :command-list/commands)))
-
-    (= content (command-str :command-list/help))
-    (when-not bot
-      (m/create-message! (:messaging @state) channel-id :content (command :command-list/help))) 
-    
-    ))
+  (when-not bot
+    (doall
+     (for [[command-key] (merge command-list command-list-hidden)]
+       (when (= content (command-str command-key))
+         (m/create-message! (:messaging @state) channel-id :content (command command-key)))))))
 
 (defn -main
   [& args]
@@ -75,7 +106,6 @@
     (e/message-pump! event-ch handle-event)
     (m/stop-connection! messaging-ch)
     (c/disconnect-bot! connection-ch)))
-
 
 (defn foo
   "I don't do a whole lot."
